056f998c31aa95346d9589b1a6e4d6ac75a3fb34
ugurkarakurt <ugur.karakurt@akinon.com>
2021-10-22 19:01:58 +0300

    #4 Feat : Styles finished.

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,16 +11,36 @@ const ecommerceBasic = (function () {
   // Setup module components
   //
 
-  // Hover Effect
+  // Variables
+  const closeBTN = document.querySelector(".close-btn");
+  const checkInp = document.getElementById("check");
+  const shopMinus = document.querySelector(".shop-minus");
+  const shopPlus = document.querySelector(".shop-plus");
+  const quantity = document.querySelector(".quantity");
+  const miniBasket = document.querySelector(".mini-basket");
+  const basket = document.querySelector(".basket");
+
+  // Quantity add-delete
+
+  const _quantity = function () {
+    shopMinus.addEventListener("click", function () {
+      const value = Number(quantity.innerHTML);
+      value > 0 && (quantity.innerHTML = value - 1)
+    });
+    shopPlus.addEventListener("click", function () {
+      const value = Number(quantity.innerHTML);
+      quantity.innerHTML = value + 1
+    })
+  }
+
+  // Slider Settings
   const _productSlider = function () {
     $(".slider").slick({
-      infinite: true,
       speed: 300,
       fade: true,
-      cssEase: 'linear'
-
-      // prevArrow: '<button class="icon-chevron_up"><</button>',
-      // nextArrow: '<button class="icon-chevron_down">></button>',
+      cssEase: 'linear',
+      prevArrow: '<button class="icon-chevron_up"><img loading="lazy" src="./assets/images/icon-previous.svg" alt="p-1"></button>',
+      nextArrow: '<button class="icon-chevron_down"><img loading="lazy" src="./assets/images/icon-next.svg" alt="p-1"></button>',
     });
     $('.product-thumbnail').on('click', function () {
       let slideIndex = $(this).data('slide');
@@ -30,6 +50,23 @@ const ecommerceBasic = (function () {
   };
 
 
+  // Sidebar close Event
+  const _closeSidebar = function () {
+    closeBTN.addEventListener("click", function () {
+      checkInp.checked = false;
+    })
+  }
+
+  // Add to cart event
+  const _addToCart = function () {}
+
+  // Toggle Basket 
+  const _toggleBasket = function () {
+    miniBasket.addEventListener("click", function () {
+      basket.classList.contains("active") ? basket.classList.remove("active") : basket.classList.add("active");
+    })
+  }
+
   //
   // Return objects assigned to module
   //
@@ -37,6 +74,10 @@ const ecommerceBasic = (function () {
   return {
     init: function () {
       _productSlider();
+      _closeSidebar();
+      _quantity();
+      _addToCart();
+      _toggleBasket();
     },
   };
 })();
